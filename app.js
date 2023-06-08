625a23b5cca240b43b0cc50c8a5314a61c4844e7
JakeKorobellis <jkorobellis@gmail.com>
2023-06-08 12:05:58 -0400

    connection to mongo db

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ app.get("/", (req,res) => {
 
 
 //mongoose connection 
+mongoose.set("strictQuery", false)
+const mongoDB = "mongodb+srv://jkorobellis:JakeKoro@blog-app.3rchsel.mongodb.net/?retryWrites=true&w=majority"
 
-
+main().catch((err) => console.log(err))
+async function main(){
+    await mongoose.connect(mongoDB)
+    console.log("MongoDB is connected")
+}
 
 app.listen(3000, () => console.log("App is listening on port 3000"))
