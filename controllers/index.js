59b94749f40f7884f41b5b83b694bdcc209407b7
JakeKorobellis <jkorobellis@gmail.com>
2023-06-08 13:17:37 -0400

    Adding messages model

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const User = require("../models/user")
-
+const Message = require("../models/message")
 //new user get
 router.get("/", (req,res) => {
 
---
+++ models/message.js
@@ -0,0 +1,11 @@
+const { ObjectId } = require("mongodb")
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const MessageSchema = new Schema({
+    message: {type: String, required: true},
+    uaer: {type: ObjectId, required: true},
+    date: {type: Date, default: Date.now}
+})
+
+module.exports = mongoose.model("Message", MessageSchema)
