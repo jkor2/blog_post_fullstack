a832de4b116ce7a7fdd42025334f9d6c10ba5733
JakeKorobellis <jkorobellis@gmail.com>
2023-06-08 13:19:06 -0400

    added 'canPost' to userschema, default: false

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
     lname: {type: String, required: true},
     email: {type: String, required: true}, 
     username: {type: String, required: true}, 
+    canPost: {type: Boolean, default: false}
 })
 
 module.exports = mongoose.model("User", UserSchema)
