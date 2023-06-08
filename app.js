8f94369c19b1651b13237d3c4e508242d6d1fb40
JakeKorobellis <jkorobellis@gmail.com>
2023-06-08 11:16:35 -0400

    Confirming connection

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const express = require("express")
 
 const app = express()
 
+app.get("/", (req,res) => {
+    res.send("Connected")
+})
+
+
+
 app.listen(3000, () => console.log("App is listening on port 3000"))
