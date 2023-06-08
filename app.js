8e4cec5181d50e35488e6911b7f921b2855e65be
JakeKorobellis <jkorobellis@gmail.com>
2023-06-08 11:57:39 -0400

    more packages

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express")
-
+const mongoose = require("mongoose")
 const app = express()
 
 app.get("/", (req,res) => {
     res.send("Connected")
 })
+
+
+
+//mongoose connection 
 
 
 
