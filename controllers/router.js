3a67588f9f6717b00f8b1b277a6a8573c99494c5
JakeKorobellis <101080670+JakeKorobellis@users.noreply.github.com>
2023-06-08 21:22:36 -0400

    Added article field in comment model

--- controllers/router.js
+++ controllers/router.js
@@ -2,11 +2,12 @@ const express = require("express")
 const router = express.Router()
 const controller = require("./index")
 
-
+//Displays nothin 
 router.get("/", controller.userGet)
-
+//Returns all posts in JSON
 router.get('/allposts', controller.postsGet)
-
+//Returns individual info on someone in JSON 
 router.get('/user/:id', controller.userInfo)
+//Returns all comments
 
 module.exports = router
