f09cdb8692436c784c551bdf78c11bdb0105c0e5
JakeKorobellis <101080670+JakeKorobellis@users.noreply.github.com>
2023-06-13 23:34:17 -0400

    Sign up form

--- frontend/src/Components/signup.js
+++ frontend/src/Components/signup.js
@@ -1,9 +1,39 @@
 import React from "react";
 
 export default function Signup() {
+    const [formData, setFormData] = React.useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+    })
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value
+        }));
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    
+        console.log(formData)
+        
+      };
+
+
+
+
     return(
-        <div>
-            <h1>This will be the sign up from, connected to the post form</h1>
+        <div className="form-hold">
+            <form onSubmit={handleSubmit} className="form">
+                <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} placeholder="John"/>
+                <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} placeholder="Doe"/>
+                <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="jdoe@gmail.com"/>
+                <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="********" />
+                <button type="submit" className="form-button">Submit</button>
+            </form>
         </div>
     )
 }
