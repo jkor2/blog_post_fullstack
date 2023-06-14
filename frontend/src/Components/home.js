18365460a8268e693fbf4cd6ee11d882b0ff27f0
JakeKorobellis <101080670+JakeKorobellis@users.noreply.github.com>
2023-06-13 22:06:48 -0400

    creating the front end

--- frontend/src/Components/home.js
+++ frontend/src/Components/home.js
@@ -3,7 +3,17 @@ import React from 'react'
 export default function Home() {
     return(
         <div className='container'>
-            
+            <div className='header'>
+                The J Word
+            </div>
+            <div className='body'>
+                <div className='ten'>
+                    Welcome to The J Word
+                </div>
+                <div className='ten'>
+                    A personal blog projecte creaetd using Node.js, Express, and React!
+                </div>
+            </div>
         </div>
     )
 }
