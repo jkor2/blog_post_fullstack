393dd95a38ddf97089ce633481c54cfc38521a77
Jake Korobellis <jkorobellis@gmail.com>
2023-06-21 23:36:26 -0400

    Loading icon

--- frontend/src/Components/posts.js
+++ frontend/src/Components/posts.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Dna } from "react-loader-spinner";
 
 export default function Posts() {
   const [data, setData] = React.useState([]);
+  const [display, setDisplay] = React.useState(false);
   const token = localStorage.getItem("token");
 
   React.useEffect(() => {
@@ -13,27 +15,43 @@ export default function Posts() {
       },
     })
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((data) => setData(data))
+      .then(setDisplay(true));
   }, []);
 
   console.log(data);
 
   return (
     <div className="container">
-      {data.status != 200 || data.status === [] ? (
-        <div>Error</div>
+      {display === false ? (
+        <div className="load-hold">
+          <Dna
+            visible={true}
+            height="1"
+            width="1"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+          />
+        </div>
       ) : (
         <div>
-          <div className="header">TEST</div>
-          <div className="body">
-            <div className="ten">TEST</div>
-            <div className="ten">TEST</div>
-            <div className="login-singup">
-              <a href="home" className="button">
-                Go Home
-              </a>
+          {data.status !== 200 ? (
+            <div>Error</div>
+          ) : (
+            <div>
+              <div className="header">TEST</div>
+              <div className="body">
+                <div className="ten">TEST</div>
+                <div className="ten">TEST</div>
+                <div className="login-singup">
+                  <a href="home" className="button">
+                    Go Home
+                  </a>
+                </div>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       )}
     </div>
