8864ada4c3a73d7d6a9afbe2cbb4838f669177cd
Jake Korobellis <jkorobellis@gmail.com>
2023-06-27 21:29:05 -0400

    Updated the post model for DB

--- models/message.js
+++ models/message.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 //Message Model
 const MessageSchema = new Schema({
   message: { type: String, required: true },
+  title: { type: String, required: true },
   uaer: { type: ObjectId, required: true },
   date: { type: Date, default: Date.now },
 });
